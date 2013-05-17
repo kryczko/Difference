c25c0193f47f824040d36575fdc560918b457518
kryczko <kevinryczko@me.com>
2013-05-17 11:58:16 -0400

    updated code with more advanced features

--- difference.cpp
+++ difference.cpp
@@ -1,23 +1,34 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+string infile;
+double unit;
+int timestep;
+
+cout << "Please enter the datafile: ";
+cin >> infile;
+cout << "What is value of the unit cell: ";
+cin >> unit;
+cout << "How many timesteps: ";
+cin >> timestep;
 ifstream datafile;
 ofstream difference;
 
 difference.open("difference.dat");
 
 
-datafile.open("coordinates");
+datafile.open(infile.c_str());
 
 int i = 0;
 
-double x_data[600], y_data[600], z_data[600];
-double H1_x[200], H2_x[200], H1_y[200], H2_y[200], H1_z[200], H2_z[200], H1[200], H2[200];
+double x_data[3*timestep], y_data[3*timestep], z_data[3*timestep];
+double H1_x[timestep], H2_x[timestep], H1_y[timestep], H2_y[timestep], H1_z[timestep], H2_z[timestep], H1[timestep], H2[timestep];
 
 while (!datafile.eof())
 {
@@ -25,7 +36,7 @@ while (!datafile.eof())
   
   i ++;
 }
-for (int n = 0; n <= 199; n ++)
+for (int n = 0; n <= timestep - 1; n ++)
 {  
   H1_x[n] = x_data[3*n + 1] - x_data[3*n];
   H2_x[n] = x_data[3*n + 2] - x_data[3*n];
@@ -34,8 +45,8 @@ for (int n = 0; n <= 199; n ++)
   H1_z[n] = z_data[3*n + 1] - z_data[3*n];
   H2_z[n] = z_data[3*n + 2] - z_data[3*n];
   
-  H1[n] =8* sqrt( H1_x[n]*H1_x[n] + H1_y[n]*H1_y[n] + H1_z[n]*H1_z[n]);
-  H2[n] =8* sqrt( H2_x[n]*H2_x[n] + H2_y[n]*H2_y[n] + H2_z[n]*H2_z[n]);
+  H1[n] =unit* sqrt( H1_x[n]*H1_x[n] + H1_y[n]*H1_y[n] + H1_z[n]*H1_z[n]);
+  H2[n] =unit* sqrt( H2_x[n]*H2_x[n] + H2_y[n]*H2_y[n] + H2_z[n]*H2_z[n]);
   
   difference << H1[n] << "\t" << H2[n] << endl;   	  
 
